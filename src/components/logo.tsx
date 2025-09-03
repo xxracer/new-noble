072e97e0c1b280036dad4b3534e30e964a1ef9c9
Maijel Cancines <maijelcancines2@gmail.com>
2025-09-03 03:05:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href="/" className={cn("flex items-center gap-2", className)}>
+    <div className={cn("flex items-center gap-2", className)}>
       <div className='bg-white p-1 rounded-md'>
         <HeartPulse className="h-7 w-7 text-primary" />
       </div>
       <span className="font-headline text-xl font-bold text-primary-foreground md:text-gray-800">
         Noble Health
       </span>
-    </Link>
+    </div>
   );
 }
